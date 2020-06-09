f68b018811fb7b82831551ed10614e717e699a39
moreenkim <moreenkim36@gmail.com>
2020-06-09 11:58:24 +0300

    fetch movie data using APIkey

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () =>{
+    const response  = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '381d0a8a',
+            s:'avengers' // string of movie we search
+        }
+    });
+    console.log(response.data);
+};
+
+fetchData();
