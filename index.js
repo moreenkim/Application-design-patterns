45749b6a50286350f49b4589bc9ce1da1504f18c
moreenkim <moreenkim36@gmail.com>
2020-06-09 14:26:43 +0300

    User innerHTML to incoorporate a div to showcase movie posters

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const fetchData = async (searchTerm) =>{
             //i:'tt0848228' //gets ID of the movie
         }
     });
-    console.log(response.data);
+    return response.data.Search;
 };
 
 const input =  document.querySelector('input');
 
-const onInput = debounce((event) => { //callback fn
-    fetchData(event.target.value);   
-});
+const onInput = async (event) => { //callback fn
+    const movies = await fetchData(event.target.value);   
+    for(let movie of movies) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+        `;
+
+        document.querySelector('#target').appendChild(div);
+    }
+};
 
 input.addEventListener('input', debounce(onInput, 500));
