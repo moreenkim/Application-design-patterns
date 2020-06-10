815415ab27bbe7a2aa147a5fe5242e5e54d1bce1
moreenkim <moreenkim36@gmail.com>
2020-06-10 15:07:25 +0300

    Show two summaries for both sides

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const autoCompleteConfig = {
         ${movie.Title}(${movie.Year})
         `;
     },
-    onOptionSelect(movie) {
-        onMovieSelect(movie);
-    },
+
     inputValue(movie){
         return movie.Title;
     },
@@ -31,24 +29,30 @@ const autoCompleteConfig = {
 createAutoComplete({
     ...autoCompleteConfig,
     root:document.querySelector('#left-autocomplete'),
-    
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie,document.querySelector('#left-summary'));
+    },
 });
 createAutoComplete({
     ...autoCompleteConfig,
     root:document.querySelector('#right-autocomplete'),
-    
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden');
+        onMovieSelect(movie,document.querySelector('#right-summary'));
+    },
 });
 
 
 
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement) => {
  const response = await axios.get('http://www.omdbapi.com/', {
     params: {
         apikey: '381d0a8a',
         i:movie.imdbID
     }
     });
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+    summaryElement.innerHTML = movieTemplate(response.data)
 };
 
 const movieTemplate =(movieDetail) => {
